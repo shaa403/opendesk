b9a82bf092843f5126ee2ceb7e962f4ff8d5c2de
David <david40au40@gmail.com>
2024-12-22 12:43:37 +0100

    ensured 2 mongo entries cannot share the same connstring, instead re-write happens

--- dscat/dscat.js
+++ dscat/dscat.js
@@ -1,18 +1,26 @@
 
 "use strict";
 
+import ds_mongo from "./datastore/mongo.js";
 import output from "./output.js";
 
 const argv = process.argv.slice(2);
 
-function mongo() {}
+function mongo() {
+   switch (argv[1]) {
+      case "addconn": ds_mongo.addconn(); break;
+      case "connect": ; break;
+      case "list": ; break;
+      case "removeconn": ; break;
+      default:ds_mongo.unknowncmd(argv[1]);	
+   } 
+}
 
 function unknowndb() {
-   console.log("Unknown database : \"%s\".", argv[0]);
+   console.log(`Unknown database : "${argv[0] || ""}".`);
    console.log("Dscat does not provide operations for the specified database.");
    output.bold("Did you mean any of the following?", true);
    console.log("\t- mongo"); /* dscat only supports mongo for now */
-   return void 0;
 }
 
 switch(argv[0]) {
